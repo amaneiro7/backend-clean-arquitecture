12cd6a002452087c5023a9a9cba2cc2bfbf5e53e
Andres Maneiro <amaneiro7@gmail.com>
2023-12-22 09:25:27 -0400

    ErrorBoundary fix

--- frontend/src/App/ErrorBoundary.tsx
+++ frontend/src/App/ErrorBoundary.tsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import { Component, type ErrorInfo, type ReactNode } from 'react'
 import ErrorPage from '../page/500'
 
-export default class ErrorBoundary extends React.Component {
-  constructor (props: React.ReactPropTypes) {
-    super(props)
-    this.state = { hasError: false }
-  }
+interface Props {
+  children: ReactNode
+}
+interface State {
+  hasError: boolean
+}
 
-  static getDerivedStateFromError (error) {
+export default class ErrorBoundary extends Component<Props, State> {
+  public state: State = { hasError: false }
+  // constructor (props: React.ReactPropTypes) {
+  //   super(props)
+  //   this.state = { hasError: false }
+  // }
+
+  public static getDerivedStateFromError (_: Error): State {
     // Update state so the next render will show the fallback UI.
     return { hasError: true }
   }
 
-  componentDidCatch (error, errorInfo) {
+  componentDidCatch (error: Error, errorInfo: ErrorInfo): void {
     // You can also log the error to an error reporting service
-    console.log(error, errorInfo)
+    console.error('error: ', error)
+    console.error('errorInfo: ', JSON.stringify(errorInfo))
+    console.error('componentStack: ', errorInfo.componentStack)
   }
 
   render () {
     if (this.state.hasError) {
       // You can render any custom fallback UI
-      return <ErrorPage />
+      return (
+        <ErrorPage />
+      )
     }
 
     return this.props.children
