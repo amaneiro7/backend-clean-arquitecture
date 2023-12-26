c2719bfe92c5a66b900096a14fd241df70b59b49
Andres Maneiro <amaneiro7@gmail.com>
2023-12-26 16:32:42 -0400

    commit

--- server/src/domain/useCases/update.useCase.ts
+++ server/src/domain/useCases/update.useCase.ts
@@ -1,19 +1,12 @@
-import { notFound } from '@hapi/boom'
 import { type Id } from '../../types/types'
 import { type UpdateRepository } from '../repositories/update.repository'
-import { type GetByIdUseCase } from './getById.useCase'
 
 export class UpdateUseCase<T, Payload> {
   constructor (
-    private readonly store: UpdateRepository<T, Payload>,
-    private readonly storeFind: GetByIdUseCase<T>
+    private readonly store: UpdateRepository<T, Payload>
   ) {}
 
   async exec (id: Id, payload: Payload): Promise<T | undefined> {
-    const itemToChange = await this.storeFind.exec({ id })
-    if (itemToChange === undefined || itemToChange === null) {
-      throw notFound()
-    }
     return await this.store.update(id, payload)
   }
 }
