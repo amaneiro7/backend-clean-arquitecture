39774625bd55c418d5b22cee59689180835ec95c
Andres Maneiro <amaneiro7@gmail.com>
2024-01-25 16:38:07 -0400

    db structure

--- server/src/Contexts/Category/domain/CategoryId.ts
+++ server/src/Contexts/Category/domain/CategoryId.ts
@@ -1,3 +1,4 @@
+// import { NumberValueObject } from '../../Shared/domain/value-object/NumberValueObject'
 import { Uuid } from '../../Shared/domain/value-object/Uuid'
 
 export class CategoryId extends Uuid {}
