fec58a816c884fe81aed3db649a322bc3ea6fcae
Andres Maneiro <amaneiro7@gmail.com>
2024-01-29 14:52:21 -0400

    Add default memory ram types and capacities (generated by blackbox.ai )

--- server/seeders/20240129184026-default_memory_ram_type.js
+++ server/seeders/20240129184026-default_memory_ram_type.js
@@ -1,25 +1,44 @@
 'use strict';
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('memory_ram_types', [
+        {
+          id: 1,
+          name: 'DDR2'
+        },
+        {
+          id: 2,
+          name: 'DDR2L'
+        },
+        {
+          id: 3,
+          name: 'DDR3'
+        },
+        {
+          id: 4,
+          name: 'DD3L'
+        },
+        {
+          id: 5,
+          name: 'DDR4'
+        },
+        {
+          id: 6,
+          name: 'DDR4L'
+        },
+        {
+          id: 7,
+          name: 'DDR5'
+        },
+        {
+          id: 8,
+          name: 'DDR5L'
+        },
+    ])
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('memory_ram_types', null, {})
   }
 };
--- server/seeders/20240129184043-default_memory_ram_capacity.js
+++ server/seeders/20240129184043-default_memory_ram_capacity.js
@@ -1,25 +1,44 @@
 'use strict';
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('memory_ram_capacities', [
+      {
+        id: 1,
+        value: 128
+      },
+      {
+        id: 2,
+        value: 256
+      },
+      {
+        id: 3,
+        value: 512
+      },
+      {
+        id: 4,
+        value: 1024
+      },
+      {
+        id: 5,
+        value: 2048
+      },
+      {
+        id: 6,
+        value: 4096
+      },
+      {
+        id: 7,
+        value: 8192
+      },
+      {
+        id: 8,
+        value: 16384
+      }      
+    ])
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('memory_ram_capacities', null, {})
   }
 };
