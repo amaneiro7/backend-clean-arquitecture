eb985d510d2a286efb6d61ef83847acba524560e
Andres Maneiro <amaneiro7@gmail.com>
2024-01-28 22:14:51 -0400

    Remove unnecessary code related to checking if a device already exists. (generated by blackbox.ai )

--- server/src/Contexts/Device/Device/application/Create/DeviceCreator.ts
+++ server/src/Contexts/Device/Device/application/Create/DeviceCreator.ts
@@ -1,8 +1,5 @@
 import { type Repository } from '../../../../Shared/domain/Repository'
 import { Device } from '../../domain/Device'
-import { DeviceActivo } from '../../domain/DeviceActivo'
-import { DeviceAlreadyExistError } from '../../domain/DeviceAlreadyExistError'
-import { DeviceSerial } from '../../domain/DeviceSerial'
 import { ValidationField } from '../ValidationField'
 
 export class DeviceCreator {
@@ -19,17 +16,5 @@ export class DeviceCreator {
     const device = Device.create({ activo, serial, statusId, modelId })
 
     await this.repository.device.save(device.toPrimitives())
-  }
-
-  private async ensureSerialDoesNotExist (name: string): Promise<void> {
-    if (await this.repository.device.searchByActivo(new DeviceActivo(name).toString()) !== null) {
-      throw new DeviceAlreadyExistError(name)
-    }
-  }
-
-  private async ensureActivoDoesNotExist (name: string): Promise<void> {
-    if (await this.repository.device.searchBySerial(new DeviceSerial(name).toString()) !== null) {
-      throw new DeviceAlreadyExistError(name)
-    }
   }
 }
