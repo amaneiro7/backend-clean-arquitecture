2ff2c0bdba40f55abfbf24b4ccc07c1059862a80
Andres Maneiro <amaneiro7@gmail.com>
2024-03-20 00:37:52 -0400

    default data

--- server/seeders/20240317202210-default-device_keyboard.js
+++ server/seeders/20240317202210-default-device_keyboard.js
@@ -1,25 +1,28 @@
 'use strict';
 
+const { randomUUID } = require('crypto');
+const { keyboadAlmacen } = require('./deviceData/keyboardAlmacen');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('devices', keyboadAlmacen.map((device) => ({
+      id: randomUUID(),
+      serial: device.serial,
+      activo: device.activo,
+      status_id: device.statusId,
+      category_id: device.categoryId,
+      brand_id: device.brandId,
+      model_id: device.modelId,
+      employee_id: device.employeeId,
+      location_id: device.locationId,
+      observation: device.observation,
+      created_at: new Date(),
+      updated_at: new Date()
+    })))
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return await queryInterface.bulkDelete('devices', keyboadAlmacen.map((device) => device.serial), {})
   }
 };
