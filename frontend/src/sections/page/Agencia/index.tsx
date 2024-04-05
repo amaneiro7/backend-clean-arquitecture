e555f0348de51d1880314de995852cf0a894b0df
Andres Maneiro <amaneiro7@gmail.com>
2024-04-05 16:31:03 -0400

    Update useInputData.ts: no changes to locationId handling
    This commit updates the file useInputData.ts in the frontend/src/sections/page/Almacen directory, but the handling of the locationId input remains unchanged. The value of locationId is still obtained from the searchParams and set to the second argument of the useInputsData hook if it exists, or to an empty string otherwise.

---
+++ frontend/src/sections/page/Agencia/index.tsx
@@ -0,0 +1,153 @@
+import { Suspense, lazy, useCallback, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAppContext } from '../../Context/AppContext'
+import { useInputsData } from './useInputData'
+import { type DevicesMappedApiResponse } from '../../../modules/shared/domain/types/responseTypes'
+import { Computer } from '../../../modules/devices/fetures/computer/domain/Computer'
+import { Operator } from '../../../modules/shared/domain/criteria/FilterOperators'
+import debounce from 'just-debounce-it'
+import { type SearchByCriteriaQuery } from '../../../modules/shared/infraestructure/criteria/SearchByCriteriaQuery'
+
+const TableHeader = lazy(async () => await import('../../components/TableHeader'))
+const DeviceTableCard = lazy(async () => await import('../../Device/device/DeviceTableCard'))
+const TableStructure = lazy(async () => await import('../../components/Table'))
+const Button = lazy(async () => await import('../../ui/button'))
+const TabsComponent = lazy(async () => await import('../../ui/tabs'))
+const BrandSelect = lazy(async () => await import('../../Device/brand/BrandSelect'))
+const SerialInput = lazy(async () => await import('../../Device/device/SerialInput'))
+const ActivoInput = lazy(async () => await import('../../Device/device/ActivoInput'))
+const ModelSelect = lazy(async () => await import('../../Device/model/ModelSelect'))
+const StatusSelect = lazy(async () => await import('../../Device/status/StatusSelect'))
+const LocationSelect = lazy(async () => await import('../../Device/location/LocationSelect'))
+
+export default function AgenciaPage () {
+  const { device: { devices }, addFilter, cleanFilters } = useAppContext()
+  const navigate = useNavigate()
+  const { inputData, updateInputData, clearInputs } = useInputsData()
+
+  const debounceGetDevices = useCallback(
+    debounce((query: SearchByCriteriaQuery) => {
+      addFilter(query)
+    }, 300)
+    , [addFilter]
+  )
+
+  const handleChange = (name: string, value: string, operator?: Operator) => {
+    const filters = [{
+      field: name,
+      operator: operator ?? Operator.EQUAL,
+      value
+    }]
+    updateInputData({ name, value })
+    debounceGetDevices({ filters })
+  }
+
+  useEffect(() => {
+    cleanFilters({
+      filters: [{
+        field: 'typeOfSite',
+        operator: Operator.EQUAL,
+        value: '2'
+      }]
+    })
+  }, [])
+
+  const handleClear = () => {
+    clearInputs()
+    cleanFilters({
+      filters: []
+    })
+  }
+
+  const defaultHeaderTitle = ['Categoria', 'Serial', 'Activo', 'Status', 'Marca', 'Modelo', 'Ubicación', 'Observaciones']
+  // const headerTitleComputer = ['Nombre', 'Procesador', 'Memoria Ram', 'Disco Duro', 'Tipo', 'Sistema Operativo', 'Arquitectura', 'Diracción MAC', 'Dirección IP']
+  const headerTitleComputer = ['Procesador', 'Memoria Ram', 'Disco Duro', 'Tipo']
+  let headerTitle: string[]
+  if (Computer.isComputerCategory({ categoryId: inputData.categoryId })) {
+    headerTitle = defaultHeaderTitle.concat(headerTitleComputer)
+  } else {
+    headerTitle = defaultHeaderTitle
+  }
+
+  return (
+    <main className='max-w-full h-full flex flex-col gap-5 p-5'>
+      <div>
+        <h1 className='text-lg text-primary font-bold'>InventarioAPP</h1>
+      </div>
+      <Suspense>
+        <Button
+          type='button'
+          text='Agregar un nuevo item'
+          actionType='ACTION'
+          handle={() => { navigate('/device/add') }}
+        />
+      </Suspense>
+        <Suspense>
+          <TabsComponent
+            value={inputData.categoryId}
+            onChange={handleChange}
+          />
+        </Suspense>
+      <header className="grid grid-cols-[repeat(auto-fit,_250px)] gap-5 place-content-center">
+        <Suspense>
+          <BrandSelect
+            value={inputData.brandId}
+            categoryId={inputData.categoryId}
+            onChange={handleChange}
+            isForm={false}
+          />
+        </Suspense>
+        <Suspense>
+          <StatusSelect
+            value={inputData.statusId}
+            onChange={handleChange}
+          />
+        </Suspense>
+        <Suspense>
+          <SerialInput
+            value={inputData.serial}
+            onChange={handleChange}
+          />
+        </Suspense>
+        <Suspense>
+          <ActivoInput
+            value={inputData.activo}
+            onChange={handleChange}
+          />
+        </Suspense>
+        <Suspense>
+          <ModelSelect
+            value={inputData.modelId}
+            brandId={inputData.brandId}
+            categoryId={inputData.categoryId}
+            onChange={handleChange}
+            isForm={false}
+          />
+        </Suspense>
+        <Suspense>
+          <LocationSelect
+            value={inputData.locationId}
+            typeOfSiteId={'3'}
+            statusId={inputData.statusId}
+            onChange={handleChange}
+            isForm={false}
+          />
+        </Suspense>
+        <Suspense>
+          <Button
+            actionType='CANCEL'
+            type='button'
+            text='Limpiar'
+            handle={handleClear}
+          />
+        </Suspense>
+      </header>
+      <Suspense>
+        <TableStructure>
+          <TableHeader headerTitle={headerTitle}/>
+          <DeviceTableCard category={inputData.categoryId} device={devices as unknown as DevicesMappedApiResponse[]}/>
+        </TableStructure>
+      </Suspense>
+    </main>
+  )
+}
