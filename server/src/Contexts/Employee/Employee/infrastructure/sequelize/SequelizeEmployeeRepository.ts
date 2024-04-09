b8580b6220a4354726c483a4d6e416037266e465
Andres Maneiro <amaneiro7@gmail.com>
2024-04-09 16:32:02 -0400

    "Add CriteriaToSequelizeConverter to SequelizeEmployeeRepository and implement matching method
    - SequelizeEmployeeRepository now extends CriteriaToSequelizeConverter and implements EmployeeRepository.
    - A new method, matching, has been added to SequelizeEmployeeRepository to support filtering of EmployeeModel by Criteria.
    - The convert method from CriteriaToSequelizeConverter is used in the matching method to convert Criteria to Sequelize options.
    - The searchAll method now returns an array of EmployeePrimitives.
    - The searchById method has been updated to return null if the EmployeeModel with the given id is not found."

--- server/src/Contexts/Employee/Employee/infrastructure/sequelize/SequelizeEmployeeRepository.ts
+++ server/src/Contexts/Employee/Employee/infrastructure/sequelize/SequelizeEmployeeRepository.ts
@@ -1,10 +1,20 @@
+import { CriteriaToSequelizeConverter } from '../../../../Shared/infrastructure/criteria/CriteriaToSequelizeConverter'
 import { type EmployeePrimitives } from '../../domain/Employee'
-import { EmployeeRepository } from '../../domain/EmployeeRepository'
+import { type EmployeeRepository } from '../../domain/EmployeeRepository'
 import { EmployeeModel } from './EmployeeSchema'
+import { type Models } from '../../../../Shared/infrastructure/persistance/Sequelize/SequelizeRepository'
+import { type Criteria } from '../../../../Shared/domain/criteria/Criteria'
+import { sequelize } from '../../../../Shared/infrastructure/persistance/Sequelize/SequelizeConfig'
 
-export class SequelizeEmployeeRepository extends EmployeeRepository {
+export class SequelizeEmployeeRepository extends CriteriaToSequelizeConverter<EmployeeModel> implements EmployeeRepository {
+  private readonly models = sequelize.models as unknown as Models
   async searchAll (): Promise<EmployeePrimitives[]> {
     return await EmployeeModel.findAll()
+  }
+
+  async matching (criteria: Criteria): Promise<EmployeePrimitives[]> {
+    const options = this.convert(criteria)
+    return await EmployeeModel.findAll(options)
   }
 
   async searchById (employeeId: string): Promise<EmployeePrimitives | null> {
