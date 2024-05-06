c97567ccf6bc36bf119e379b7fe4b4bfa15321b2
Andres Maneiro <amaneiro7@gmail.com>
2024-05-06 08:27:45 -0400

    "Update seeder file to bulk insert data for model computers and laptops in separate tables"
    This commit updates the seeder file to bulk insert data for model computers and laptops in separate tables (model\_computers and model\_l

--- server/seeders/20240129191852-default_models.js
+++ server/seeders/20240129191852-default_models.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const {categoryData, categoryOnlyNames} = require('./categoryData/categoryData');
-const { modelComputer: models } = require('./modelsSeries/modelComputer'); 
-  
+const { categoryData, categoryOnlyNames } = require('./categoryData/categoryData');
+const { modelComputer: models } = require('./modelsSeries/modelComputer');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {    
+  async up (queryInterface, Sequelize) {
     const modelComputer = models.filter(model => {
       const computer = categoryData.filter(category => category.name === categoryOnlyNames.COMPUTADORAS)[0].id.includes(model.categoryId)
       const servidores = categoryData.filter(category => category.name === categoryOnlyNames.SERVIDORES)[0].id.includes(model.categoryId)
@@ -14,19 +14,19 @@ module.exports = {
         computer, servidores, allInOne
       }
     })
-    const modelLaptop = models.filter(model => categoryData.filter(category => category.name === categoryOnlyNames.LAPTOPS)[0].id.includes(model.categoryId))
-      return Promise.all([
-        queryInterface.bulkInsert('models', models.map(({id, name, categoryId, brandId}) => ({
+    try {
+      return await queryInterface.sequelize.transaction(async (t) => {
+        await queryInterface.bulkInsert('models', models.map(({ id, name, categoryId, brandId }) => ({
           id,
           name,
           category_id: categoryId,
           brand_id: brandId,
           created_at: new Date(),
           updated_at: new Date()
-        }))),
-        queryInterface.bulkInsert('model_computers', modelComputer.map(({id, categoryId, processorSocketId, memoryRamTypeId, memoryRamSlotQuantity, hasBluetooth, hasDVI, hasHDMI, hasVGA, hasWifiAdapter}) => ({
+        })), { transaction: t })
+        await queryInterface.bulkInsert('model_computers', modelComputer.map(({ id, categoryId, processorSocketId, memoryRamTypeId, memoryRamSlotQuantity, hasBluetooth, hasDVI, hasHDMI, hasVGA, hasWifiAdapter }) => ({
           id,
-          model_series_id: id,        
+          model_series_id: id,
           category_id: categoryId,
           processor_socket_id: processorSocketId,
           memory_ram_type_id: memoryRamTypeId,
@@ -38,25 +38,27 @@ module.exports = {
           has_wifi_adapter: hasWifiAdapter,
           created_at: new Date(),
           updated_at: new Date()
-        })
-      )),
-      queryInterface.bulkInsert('model_laptops', modelLaptop.map(({id, categoryId, processorSocketId, memoryRamTypeId, memoryRamSlotQuantity, hasBluetooth, hasDVI, hasHDMI, hasVGA, hasWifiAdapter, batteryModel}) => ({
-        id,
-        model_series_id: id,        
-        category_id: categoryId,
-        processor_socket_id: processorSocketId,
-        memory_ram_type_id: memoryRamTypeId,
-        memory_ram_slot_quantity: memoryRamSlotQuantity,
-        has_bluetooth: hasBluetooth,
-        has_dvi: hasDVI,
-        has_hdmi: hasHDMI,
-        has_vga: hasVGA,
-        has_wifi_adapter: hasWifiAdapter,
-        battery_model: batteryModel,
-        created_at: new Date(),
-        updated_at: new Date()
-      })))
-    ])
+        })), { transaction: t })
+        await queryInterface.bulkInsert('model_laptops', modelLaptop.map(({ id, categoryId, processorSocketId, memoryRamTypeId, memoryRamSlotQuantity, hasBluetooth, hasDVI, hasHDMI, hasVGA, hasWifiAdapter, batteryModel }) => ({
+          id,
+          model_series_id: id,
+          category_id: categoryId,
+          processor_socket_id: processorSocketId,
+          memory_ram_type_id: memoryRamTypeId,
+          memory_ram_slot_quantity: memoryRamSlotQuantity,
+          has_bluetooth: hasBluetooth,
+          has_dvi: hasDVI,
+          has_hdmi: hasHDMI,
+          has_vga: hasVGA,
+          has_wifi_adapter: hasWifiAdapter,
+          battery_model: batteryModel,
+          created_at: new Date(),
+          updated_at: new Date()
+        })), { transaction: t })
+      })
+    } catch (error) {
+      throw new Error(error)
+    }
   },
 
   async down (queryInterface, Sequelize) {
