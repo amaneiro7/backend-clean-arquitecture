b08331e134b6f6512212b70224e70b41cb8aa09c
Andres Maneiro <amaneiro7@gmail.com>
2024-05-16 03:59:58 -0400

    update package

--- frontend/src/sections/mui/NumberInput.tsx
+++
@@ -1,3 +0,0 @@
-export { default as Unstable_NumberInput } from '@mui/base/Unstable_NumberInput'
-export type { NumberInputProps } from '@mui/base/Unstable_NumberInput'
-export type { NumberInputOwnerState } from '@mui/base/Unstable_NumberInput'
