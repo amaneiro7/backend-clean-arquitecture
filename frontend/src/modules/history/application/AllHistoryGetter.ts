c40551482c8742612a37687dd634f0e9bd7671e5
Andres Maneiro <amaneiro7@gmail.com>
2024-07-03 13:41:34 -0400

    "Rename 'StatePrimitives' to 'HistoryPrimitives' and 'StateRepository' to 'HistoryRepository' in AllHistoryGetter class."

--- frontend/src/modules/history/application/AllHistoryGetter.ts
+++ frontend/src/modules/history/application/AllHistoryGetter.ts
@@ -1,9 +1,9 @@
-import { type StatePrimitives } from '../domain/history'
-import { StateRepository } from '../domain/HistoryRepository'
-export class AllStateGetter {
-  constructor(private readonly repository: StateRepository) { }
+import { type HistoryPrimitives } from '../domain/history'
+import { HistoryRepository } from '../domain/HistoryRepository'
+export class AllHistoryGetter {
+  constructor(private readonly repository: HistoryRepository) { }
 
-  async get(): Promise<StatePrimitives[]> {
+  async get(): Promise<HistoryPrimitives[]> {
     return await this.repository.getAll()
   }
 }
