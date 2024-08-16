39eb75751ea2a371b1625d4cdf82000a28cb5b72
Andres Maneiro <amaneiro7@gmail.com>
2024-08-16 12:25:50 -0400

    Added optional chaining (?.) to several property accesses in DeviceTableRow components to prevent null pointer exceptions.

--- frontend/src/sections/page/ListWrapper/ListComputer/DeviceTableRow.tsx
+++ frontend/src/sections/page/ListWrapper/ListComputer/DeviceTableRow.tsx
@@ -15,27 +15,27 @@ interface Props {
 export const DeviteRowTable = memo(({data: devices, style, index}: Props) => {
     return (
       <TableRow style={style} key={index}>
-        <TableCellEditDeleteIcon typeOfSiteId={devices[index].location.typeOfSiteId} stateId={devices[index].id} state={devices[index]} url={`/device/edit/${devices[index].id}`} />
-        <TableCell size='w-28' value={devices[index].employee?.userName} url={`/employee/edit/${devices[index].employeeId}`} />
-        <TableCell size='w-52' value={devices[index].location?.name} />
+        <TableCellEditDeleteIcon typeOfSiteId={devices[index]?.location?.typeOfSiteId} stateId={devices[index].id} state={devices[index]} url={`/device/edit/${devices[index].id}`} />
+        <TableCell size='w-28' value={devices[index]?.employee?.userName} url={`/employee/edit/${devices[index].employeeId}`} />
+        <TableCell size='w-52' value={devices[index]?.location?.name} />
         <TableCell size='w-28' value={devices[index]?.computer?.ipAddress} />
         <TableCell size='w-32' value={devices[index]?.serial ?? "Sin Serial"} state={devices[index]} url={`/device/edit/${devices[index].id}`} />
-        <TableCell size='w-20' value={devices[index].status?.name} />
-        <TableCell size='w-28' value={devices[index].category?.name} />
-        <TableCell size='w-32' value={devices[index].brand?.name} />
-        <TableCell size='w-52' value={devices[index].model?.name} />
+        <TableCell size='w-20' value={devices[index]?.status?.name} />
+        <TableCell size='w-28' value={devices[index]?.category?.name} />
+        <TableCell size='w-32' value={devices[index]?.brand?.name} />
+        <TableCell size='w-52' value={devices[index]?.model?.name} />
         <TableCell size='w-52' value={devices[index]?.computer?.computerName} />
         <TableCell size='w-52' value={devices[index]?.computer ? `${devices[index]?.computer?.processor?.productCollection} ${devices[index]?.computer?.processor?.numberModel}` : ""} />
         <TableCell size='w-24' value={devices[index]?.computer ? `${devices[index]?.computer?.memoryRamCapacity} Gb` : ""} />
         <TableCell size='w-24' value={devices[index]?.computer ? devices[index]?.computer?.memoryRam?.map((mem) => mem).join(" / ") : ""} />
         <TableCell
           size='w-32'
-          value={devices[index]?.model?.modelComputer ? devices[index]?.model?.modelComputer.memoryRamType?.name : devices[index]?.model?.modelLaptop ? devices[index]?.model?.modelLaptop?.memoryRamType?.name : ""}
+          value={devices[index]?.model?.modelComputer ? devices[index]?.model?.modelComputer?.memoryRamType?.name : devices[index]?.model?.modelLaptop ? devices[index]?.model?.modelLaptop?.memoryRamType?.name : ""}
         />
         <TableCell size='w-20' value={devices[index]?.computer?.hardDriveCapacity ? `${devices[index]?.computer?.hardDriveCapacity?.name} Gb` : ""} />
         <TableCell size='w-20' value={devices[index]?.computer?.hardDriveType?.name} />
         <TableCell size='w-32' value={devices[index]?.computer?.operatingSystem?.name} />
         <TableCell size='w-32' value={devices[index]?.computer?.operatingSystemArq?.name} />
-        <TableCell size='w-52' value={devices[index].observation} />
+        <TableCell size='w-52' value={devices[index]?.observation} />
       </TableRow>
 )}, areEqual)
