25faf9f8d2834322f3c75523c208458540a38023
Andres Maneiro <amaneiro7@gmail.com>
2024-09-24 16:35:27 -0400

    "Refactored LocationInputs and ModelInputs components, reformatted code, and rearranged elements"

--- frontend/src/sections/page/FormLocation/LocationInputs.tsx
+++ frontend/src/sections/page/FormLocation/LocationInputs.tsx
@@ -5,13 +5,13 @@ import { type OnHandleChange } from "@/modules/shared/domain/types/types"
 import { type FormLocationDisabled, type FormLocationErrors, type FormLocationRequired, type DefaultLocationProps } from "@/sections/Hooks/locations/DefaulLocationtInitialState"
 
 interface Props {
-    isAddForm: boolean
-    formData: DefaultLocationProps
-    error: FormLocationErrors
-    disabled: FormLocationDisabled
-    required: FormLocationRequired
-    onChange: OnHandleChange
-    handleSite?: (value: string, siteName: string) => void
+  isAddForm: boolean
+  formData: DefaultLocationProps
+  error: FormLocationErrors
+  disabled: FormLocationDisabled
+  required: FormLocationRequired
+  onChange: OnHandleChange
+  handleSite?: (value: string, siteName: string) => void
 }
 
 const TypeOfSiteComboBox = lazy(async () => import('@/sections/components/combo_box/TypeOfSiteComboBox').then(m => ({ default: m.TypeOfSiteComboBox })))
@@ -24,104 +24,103 @@ const LocationNameInput = lazy(async () => import('@/sections/components/text-in
 const CodeAgencyInput = lazy(async () => import('@/sections/components/number-inputs/CodeAgency').then(m => ({ default: m.CodeAgencyInput })))
 
 export function LocationInputs({ onChange, handleSite, formData, isAddForm, disabled, error, required }: Props) {
-  console.log(formData)
-    return (
-      <>
+  return (
+    <>
+      <Suspense fallback={<InputSkeletonLoading />}>
+        <TypeOfSiteComboBox
+          isAddForm={isAddForm}
+          onChange={onChange}
+          value={formData.typeOfSiteId}
+          type='form'
+        />
+      </Suspense>
+      <div className='flex gap-4'>
         <Suspense fallback={<InputSkeletonLoading />}>
-          <TypeOfSiteComboBox
+          <RegionComboBox
             isAddForm={isAddForm}
             onChange={onChange}
-            value={formData.typeOfSiteId}
             type='form'
+            disabled={disabled.regionId}
+            required={required.regionId}
+            error={error.regionId}
+            value={formData.regionId}
           />
         </Suspense>
-        <div className='flex gap-4'>
-          <Suspense fallback={<InputSkeletonLoading />}>
-            <RegionComboBox
-              isAddForm={isAddForm}
-              onChange={onChange}
-              type='form'
-              disabled={disabled.regionId}
-              required={required.regionId}
-              error={error.regionId}
-              value={formData.regionId}
-            />
-          </Suspense>
-          <Suspense fallback={<InputSkeletonLoading />}>
-            <StateComboBox
-              isAddForm={isAddForm}
-              onChange={onChange}
-              type='form'
-              value={formData.stateId}
-              region={formData.regionId}
-              disabled={disabled.stateId}
-              required={required.stateId}
-              error={error.stateId}
-            />
-          </Suspense>
-        </div>
-        <div className='flex gap-4'>
-          <Suspense fallback={<InputSkeletonLoading />}>
-            <CityComboBox
-              isAddForm={isAddForm}
-              onChange={onChange}
-              type='form'
-              value={formData.cityId}
-              state={formData.stateId}
-              disabled={disabled.cityId}
-              required={required.cityId}
-              error={error.cityId}
-            />
-          </Suspense>
-          <Suspense fallback={<InputSkeletonLoading />}>
-            <SiteComboBox
-              isAddForm={isAddForm}
-              handleSite={handleSite}
-              type='form'
-              value={formData.siteId}
-              city={formData.cityId}
-              disabled={disabled.siteId}
-              required={required.siteId}
-              error={error.siteId}
-            />
-          </Suspense>
-        </div>
-        <div className='flex gap-4'>
-          {(isAddForm && formData.typeOfSiteId === TypeOfSiteId.SitesOptions.AGENCY) ?
-            <Suspense>
-              <CodeAgencyInput
-                onChange={onChange}
-                value={formData.codeAgency}
-                disabled={disabled.codeAgency}
-                error={error.codeAgency}
-                required={required.codeAgency}                
-              />
-            </Suspense> 
-                  : null}
-          <Suspense fallback={<InputSkeletonLoading />}>
-            <LocationNameInput
-              onChange={onChange}
-              value={formData.name}
-              siteName={formData.siteName}
-              typeOfSite={formData.typeOfSiteId}
-              type='form'
-              isAddForm={isAddForm}
-              disabled={disabled.name}
-              required={required.name}
-              error={error.name}
-            />
-          </Suspense>
-        </div>
-
         <Suspense fallback={<InputSkeletonLoading />}>
-          <SubnetInput
+          <StateComboBox
+            isAddForm={isAddForm}
             onChange={onChange}
-            value={formData.subnet}
-            disabled={disabled.subnet}
-            required={required.subnet}
-            error={error.subnet}
+            type='form'
+            value={formData.stateId}
+            region={formData.regionId}
+            disabled={disabled.stateId}
+            required={required.stateId}
+            error={error.stateId}
           />
         </Suspense>
-      </>
-    )
+      </div>
+      <div className='flex gap-4'>
+        <Suspense fallback={<InputSkeletonLoading />}>
+          <CityComboBox
+            isAddForm={isAddForm}
+            onChange={onChange}
+            type='form'
+            value={formData.cityId}
+            state={formData.stateId}
+            disabled={disabled.cityId}
+            required={required.cityId}
+            error={error.cityId}
+          />
+        </Suspense>
+        <Suspense fallback={<InputSkeletonLoading />}>
+          <SiteComboBox
+            isAddForm={isAddForm}
+            handleSite={handleSite}
+            type='form'
+            value={formData.siteId}
+            city={formData.cityId}
+            disabled={disabled.siteId}
+            required={required.siteId}
+            error={error.siteId}
+          />
+        </Suspense>
+      </div>
+      <div className='flex gap-4'>
+        {(isAddForm && formData.typeOfSiteId === TypeOfSiteId.SitesOptions.AGENCY) ?
+          <Suspense>
+            <CodeAgencyInput
+              onChange={onChange}
+              value={formData.codeAgency}
+              disabled={disabled.codeAgency}
+              error={error.codeAgency}
+              required={required.codeAgency}
+            />
+          </Suspense>
+          : null}
+        <Suspense fallback={<InputSkeletonLoading />}>
+          <LocationNameInput
+            onChange={onChange}
+            value={formData.name}
+            siteName={formData.siteName}
+            typeOfSite={formData.typeOfSiteId}
+            type='form'
+            isAddForm={isAddForm}
+            disabled={disabled.name}
+            required={required.name}
+            error={error.name}
+          />
+        </Suspense>
+      </div>
+
+      <Suspense fallback={<InputSkeletonLoading />}>
+        <SubnetInput
+          onChange={onChange}
+          value={formData.subnet}
+          disabled={disabled.subnet}
+          required={required.subnet}
+          error={error.subnet}
+        />
+      </Suspense>
+    </>
+  )
 }
--- frontend/src/sections/page/FormModel/ModelFeatures.tsx
+++ frontend/src/sections/page/FormModel/ModelFeatures.tsx
@@ -8,13 +8,13 @@ import { InputSkeletonLoading } from '@/sections/components/skeleton/inputSkelet
 import { type OnHandleChange } from "@/modules/shared/domain/types/types"
 import { type DefaultModelProps, type FormModelDisabled, type FormModelErrors, type FormModelRequired } from '@/sections/Hooks/model/DefaultInitialModelState'
 
-interface Props {    
-    formData: DefaultModelProps
-    onChange: OnHandleChange
-    isAddForm?: boolean
-    disabled: FormModelDisabled
-    error: FormModelErrors
-    required: FormModelRequired
+interface Props {
+  formData: DefaultModelProps
+  onChange: OnHandleChange
+  isAddForm?: boolean
+  disabled: FormModelDisabled
+  error: FormModelErrors
+  required: FormModelRequired
 }
 
 const Input = lazy(async () => import('@/sections/components/text-inputs/Input').then(m => ({ default: m.Input })))
@@ -30,110 +30,109 @@ export function ModelInputs({ onChange, formData, isAddForm, disabled, error, re
   const categoryType = useMemo(() => {
     return (
       ModelComputer.isComputerCategory({ categoryId: formData.categoryId }) ? 'computer' :
-      ModelLaptop.isLaptopCategory({ categoryId: formData.categoryId }) ? 'laptop' :
-      ModelMonitor.isMonitorCategory({ categoryId: formData.categoryId }) ? 'monitor' :
-      ModelPrinter.isPrinterCategory({ categoryId: formData.categoryId }) ? 'printer' :
-      ModelKeyboard.isKeyboardCategory({ categoryId: formData.categoryId }) ? 'keyboard' : null
+        ModelLaptop.isLaptopCategory({ categoryId: formData.categoryId }) ? 'laptop' :
+          ModelMonitor.isMonitorCategory({ categoryId: formData.categoryId }) ? 'monitor' :
+            ModelPrinter.isPrinterCategory({ categoryId: formData.categoryId }) ? 'printer' :
+              ModelKeyboard.isKeyboardCategory({ categoryId: formData.categoryId }) ? 'keyboard' : null
     )
   }, [formData.categoryId])
-    return (
-      <>
-        <Suspense fallback={<InputSkeletonLoading />}>
-          <CategoryComboBox
-            value={formData.categoryId}
-            onChange={onChange}
-            type='form'
-            isAdd={isAddForm}
-            error={error.categoryId}
-            isDisabled={disabled.categoryId}
-            isRequired={required.categoryId}
-          />
-        </Suspense>
-        <Suspense fallback={<InputSkeletonLoading />}>
-          <BrandComboBox
-            value={formData.brandId}
-            onChange={onChange}
-            type='form'
-            isAdd={isAddForm}
-            error={error.brandId}
-            categoryId={formData.categoryId}
-            isDisabled={disabled.brandId}
-            isRequired={required.brandId}
-          />
-
-        </Suspense>
-        <Suspense fallback={<InputSkeletonLoading />}>
-          <Input
-            id='name'
-            name='name'
-            type='text'
-            label='Name'
-            onChange={(event) => {
-              const { name, value } = event.target
-              onChange(name, value)
-            }}
-            value={formData.name}            
-            error={!!error.name}
-            errorMessage={error.name}
-            disabled={disabled.name}
-            required={required.name}
-          />
-        </Suspense>
-        <Checkbox
-          label='modelo genéirco'
-          text='¿Es un modelo genérico?'
-          name='generic'
-          value={formData.generic ?? false}
-          handle={(event) => {
-              const { name, checked } = event.target
-              onChange(name, checked);
-          }}
+  return (
+    <>
+      <Suspense fallback={<InputSkeletonLoading />}>
+        <CategoryComboBox
+          value={formData.categoryId}
+          onChange={onChange}
+          type='form'
+          isAdd={isAddForm}
+          error={error.categoryId}
+          isDisabled={disabled.categoryId}
+          isRequired={required.categoryId}
         />
-        {categoryType === 'computer' || categoryType === 'laptop' ? 
-          <Suspense>
-            <AddModelComputer 
-              formData={formData} 
-              onChange={onChange}
-              error={error}
-              disabled={disabled}
-              required={required}
-              categoryType={categoryType}
-            />
-          </Suspense> 
-          : null}
-        {categoryType === 'monitor' ? 
-          <Suspense>
-            <AddModelMonitor 
-              formData={formData} 
-              onChange={onChange}
-              error={error}
-              disabled={disabled}
-              required={required}
-            />
-          </Suspense>
-          : null}
-        {categoryType === 'printer' ? 
-          <Suspense>
-            <AddModelPrinter 
-              formData={formData} 
-              onChange={onChange}
-              error={error}
-              disabled={disabled}
-              required={required}
-            />
-          </Suspense>
-          : null}
-        {categoryType === 'keyboard' ? 
-          <Suspense>
-            <AddModelKeyboard 
-              formData={formData} 
-              onChange={onChange}
-              error={error}
-              disabled={disabled}
-              required={required}
-            />
-          </Suspense>
-          : null}
-      </>
-    )
+      </Suspense>
+      <Suspense fallback={<InputSkeletonLoading />}>
+        <BrandComboBox
+          value={formData.brandId}
+          onChange={onChange}
+          type='form'
+          isAdd={isAddForm}
+          error={error.brandId}
+          isDisabled={disabled.brandId}
+          isRequired={required.brandId}
+        />
+
+      </Suspense>
+      <Suspense fallback={<InputSkeletonLoading />}>
+        <Input
+          id='name'
+          name='name'
+          type='text'
+          label='Name'
+          onChange={(event) => {
+            const { name, value } = event.target
+            onChange(name, value)
+          }}
+          value={formData.name}
+          error={!!error.name}
+          errorMessage={error.name}
+          disabled={disabled.name}
+          required={required.name}
+        />
+      </Suspense>
+      <Checkbox
+        label='modelo genéirco'
+        text='¿Es un modelo genérico?'
+        name='generic'
+        value={formData.generic ?? false}
+        handle={(event) => {
+          const { name, checked } = event.target
+          onChange(name, checked);
+        }}
+      />
+      {categoryType === 'computer' || categoryType === 'laptop' ?
+        <Suspense>
+          <AddModelComputer
+            formData={formData}
+            onChange={onChange}
+            error={error}
+            disabled={disabled}
+            required={required}
+            categoryType={categoryType}
+          />
+        </Suspense>
+        : null}
+      {categoryType === 'monitor' ?
+        <Suspense>
+          <AddModelMonitor
+            formData={formData}
+            onChange={onChange}
+            error={error}
+            disabled={disabled}
+            required={required}
+          />
+        </Suspense>
+        : null}
+      {categoryType === 'printer' ?
+        <Suspense>
+          <AddModelPrinter
+            formData={formData}
+            onChange={onChange}
+            error={error}
+            disabled={disabled}
+            required={required}
+          />
+        </Suspense>
+        : null}
+      {categoryType === 'keyboard' ?
+        <Suspense>
+          <AddModelKeyboard
+            formData={formData}
+            onChange={onChange}
+            error={error}
+            disabled={disabled}
+            required={required}
+          />
+        </Suspense>
+        : null}
+    </>
+  )
 }
