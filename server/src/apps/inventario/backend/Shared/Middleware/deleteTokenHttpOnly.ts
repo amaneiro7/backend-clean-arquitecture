c1eae334400c4283507ba486aa6f4a816f3abce6
Andres Maneiro <amaneiro7@gmail.com>
2024-09-27 01:02:46 -0400

    se actualizo el clearCokkie ya que la el option.expires esta deprecado

--- server/src/apps/inventario/backend/Shared/Middleware/deleteTokenHttpOnly.ts
+++ server/src/apps/inventario/backend/Shared/Middleware/deleteTokenHttpOnly.ts
@@ -4,7 +4,7 @@ import httpStatus from 'http-status'
 export const deleteTokenHttpOnly = (req: Request, res: Response, next: NextFunction): void => {
   res
     .status(httpStatus.OK)
-    .clearCookie('accessToken', { httpOnly: true, expires: new Date(0) })
+    .clearCookie('accessToken')
     .json({ message: 'Token Succesfully Removed' })
 
   next()
