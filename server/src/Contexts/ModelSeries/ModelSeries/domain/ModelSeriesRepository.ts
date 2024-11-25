04a8a6537566d4271c75d799edbbf11f75892407
Andres Maneiro <amaneiro7@gmail.com>
2024-11-25 00:52:47 -0400

    "Updated ModelSeriesRepository interface: changed import path for CategoryId, removed whitespace and reformatted code."

--- server/src/Contexts/ModelSeries/ModelSeries/domain/ModelSeriesRepository.ts
+++ server/src/Contexts/ModelSeries/ModelSeries/domain/ModelSeriesRepository.ts
@@ -1,22 +1,22 @@
-import { type CategoryId } from '../../../Category/domain/CategoryId'
 import { Criteria } from '../../../Shared/domain/criteria/Criteria'
+import { type CategoryId } from '../../../Category/SubCategory/domain/CategoryId'
 import { type Primitives } from '../../../Shared/domain/value-object/Primitives'
 import { type ModelSeriesPrimitives } from './ModelSeries'
 import { type ModelSeriesId } from './ModelSeriesId'
 import { type ModelSeriesName } from './ModelSeriesName'
 
 export abstract class ModelSeriesRepository {
-  abstract save (payload: ModelSeriesPrimitives): Promise<void>
+  abstract save(payload: ModelSeriesPrimitives): Promise<void>
 
-  abstract searchAll (): Promise<ModelSeriesPrimitives[]>
-  
-  abstract matching (criteria: Criteria): Promise<ModelSeriesPrimitives[]>
+  abstract searchAll(): Promise<ModelSeriesPrimitives[]>
 
-  abstract searchById (id: Primitives<ModelSeriesId>): Promise<ModelSeriesPrimitives | null>
+  abstract matching(criteria: Criteria): Promise<ModelSeriesPrimitives[]>
 
-  abstract searchByName (name: Primitives<ModelSeriesName>): Promise<ModelSeriesPrimitives | null>
+  abstract searchById(id: Primitives<ModelSeriesId>): Promise<ModelSeriesPrimitives | null>
 
-  abstract searchByCategory (categoryId: Primitives<CategoryId>): Promise<ModelSeriesPrimitives[]>
+  abstract searchByName(name: Primitives<ModelSeriesName>): Promise<ModelSeriesPrimitives | null>
 
-  abstract remove (id: Primitives<ModelSeriesId>): Promise<void>
+  abstract searchByCategory(categoryId: Primitives<CategoryId>): Promise<ModelSeriesPrimitives[]>
+
+  abstract remove(id: Primitives<ModelSeriesId>): Promise<void>
 }
